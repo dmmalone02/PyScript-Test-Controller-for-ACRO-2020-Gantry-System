9c26312a63a07e1b8eef96ce28b7bae3dfd44aa6
David Malone <95652778+dmmalone02@users.noreply.github.com>
2025-06-27 15:31:31 -0400

    Update Tc_socketio.py
    - sio.wait() didn't allow me to be able to progress through main()
    - Need to figure out how GRBL will read the g-code emitted frm client

--- Tc_socketio.py
+++ Tc_socketio.py
@@ -5,36 +5,34 @@ import time
 
 sio = socketio.Client(logger=False, engineio_logger=False)
 
+## Socket.io Decorator Functions ##
 @sio.event 
 def connect():
-    print("Connected to OpenBuilds CONTROL")
-    
+    print("Connected to OpenBuilds CONTROL") # Connection established
+
 @sio.event
 def disconnect(reason):
-    print("\nDisconnected from Openbuilds CONTROL! reason:", reason)
+    print("\nDisconnected from Openbuilds CONTROL! reason:", reason)  # Disconnecttion detected
     
 @sio.event
 def connect_error(data):
-    print("The connection failed!")
+    print("The connection failed!") # Failed connection
    
-@sio.on('console')
-def handle_serial(data):
-    line = str(data).strip()
-    print("Console: " + line)
-
 @sio.on('*')
 def catch_all(event, data):
-    time.sleep(2)
-    log(f"Recieved event: {event} with data: {data} \n")
+    if not event.startswith('sysinfo'):
+        time.sleep(2)
+        log(f"Recieved event: {event} with data: {data} \n")
 
-@sio.on('status')
+       
+@sio.on('status')  # Retrieve machine position and state
 def handle_status(data):
     machine = data.get('machine', {})
     state = machine.get('activeState') or machine.get('modals', {}).get('spindlestate', 'Unknown')
     position = machine.get('position', {}).get('work')  
 
-    # Write state and position to external text file
-    if position:
+    
+    if position:    # Write state and position to external text file
         try:
             x = float(position.get('x', 0.0))
             y = float(position.get('y', 0.0))
@@ -47,21 +45,21 @@ def handle_status(data):
     else:
         print(f"[{datetime.datetime.now()}] State: {state} | Position: Unknown")
 
+#-----------------------------------------------------------------------------------------------------------#
+
 # Time logger function
 def log(msg):
     print(f"[{datetime.datetime.now()}] {msg}")
 
 # Function to send a single command to the BlackBox Controller
 def send_command(cmd):
-    sio.emit('gcode', {'code': cmd})
+    sio.call('data', {'data': cmd}, timeout=10) # Added timeout to see if the server is even receiving anything
     log(f"Sent: {cmd}")
-
-# Send object to limit switches and set as origin
+    
+# Set zero
 def set_zero():
-    send_command('G28')
-    time.sleep(10)
-    send_command('')
-
+    send_command('$J=G91G21Y100F5000')
+    time.sleep(2)
     log("Zeroing complete")
 
 # We can try uploading a G-code file to the OpenBuildsCONTROL GUI and see if it will open and run it
@@ -75,11 +73,17 @@ def gcode_file_upload(gfile):
 def main():
     try:
         sio.connect('http://localhost:3000')  # Connecting to controller over port 3000
-        sio.wait()
+        
     except Exception as e:
         log(f"Exception: {e}" )
-    gfile = 'gfile.gcode'
-    time.sleep(5)
-    gcode_file_upload(gfile)
-
+        
+    time.sleep(2)
+    print("status: connected")
+    time.sleep(10)
+    print("status: setting to zero")
+    set_zero()
+    print("status: coordinates set to zero")
+   # gfile = 'gfile.gcode'
+    #gcode_file_upload(gfile)
+    sio.wait()
 main()
